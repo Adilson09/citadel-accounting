a2529cc6792f75bf32795b6070a451365b31fd13
kellymweu <mweukelly@gmail.com>
2024-09-10 09:50:42 +0300

    refactor banking admin

--- backend/citadel/banking/admin.py
+++ backend/citadel/banking/admin.py
@@ -2,5 +2,8 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(Account)
+admin.site.register(BankAccount)
+admin.site.register(Cheque)
 admin.site.register(Transaction)
+admin.site.register(Transfer)
+admin.site.register(MpesaAccount)
--- backend/citadel/banking/views.py
+++ backend/citadel/banking/views.py
@@ -1,5 +1,5 @@
 from rest_framework import generics
-from .models import Account, Transaction
+from .models import *
 from .serializers import AccountSerializer, TransactionSerializer
 
 class AccountListCreateView(generics.ListCreateAPIView):
